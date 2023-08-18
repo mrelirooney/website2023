7bc1d38f0cf3969746db806c208843b50c13d8e7
mrelirooney <mrelirooney@gmail.com>
2023-08-18 07:48:14 -0400

    '' url path is not possible I guess
    I tried '', ' ', '/' , '*' , '*/' and they all do not work...I guess I need to just type in localhost:8000/admin to access my database...that sucks, but I'll survive, I guess...I mean, it's an extra step or whatever but it's all good in the neighborhood...I guess it will be harder for anyone to find...and when they do, they have to guess my username and password...speaking for which, I just changed that in the last push too...They are never going to guess that lol.

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,6 @@ from backend import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('tgims/', views.tgim_list),
     path('tgims/<int:id>', views.tgim_detail)
